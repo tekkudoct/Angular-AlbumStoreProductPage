4e4a45fd033c71b16554aa52e9fe448567a51aa0
tekkudoct <tekku.me@outlook.com>
2019-10-28 10:42:49 +0300

    ProductList component added

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 // Interfaces
 import { Album} from './album';
+import { Product } from './product';
 
 import 'rxjs/Observable';
 import 'rxjs/add/operator/map';
@@ -10,11 +11,16 @@ import {Observable} from 'rxjs/Observable';
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) {
   }
 
   getAlbum(id: number) : Observable<Album> {
     return this._http.get(this._albumUrl).map(response => <Album>response.json());
+  }
+
+  getProducts() : Observable<Product[]> {
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json())
   }
 }
